f21165e4d8a83733a0d16292a9b5b6effebeaba6
Mario Menjívar <mariomenjr@gmail.com>
2021-04-08 21:58:26 -0700

    Get sharing link from query

--- controllers/firebase.controller.js
+++ controllers/firebase.controller.js
@@ -6,19 +6,32 @@ const linkSharedTemplate = require("../templates/linkShared.json");
 
 async function share(req, res) {
   try {
+    const {
+      registrationToken,
+      sharedLink
+    } = req.query;
+
+    console.debug({ registrationToken, sharedLink });
+
     const payload = templater(linkSharedTemplate, {
       sender: `Mario`,
       origin: `OnePlus 8`,
       what: `YouTube video`,
-      link: `https://mariomenjr.com/blog/`,
+      link: sharedLink,
     });
 
-    const resp = await firebase.messaging().sendToDevice(req.query.registrationToken, payload);
+    const resp = await firebase.messaging().sendToDevice(registrationToken, payload);
     return res.send(resp);
   } catch (error) {
     console.error(`[error] ${error}`);
     return res.status(500).send(error);
   }
 }
+
+// OnePlus 8
+// cI8zqkYeRMaUTDmVC93Dlk:APA91bFoUYDptKR7tqG9mXDRx514-6eovvTgUKL6L8F26ISsBiKH6lQc4RlKSWoKEUIOQSAsf1_LWES4hw3u-bvPFdoDOWX_HJXYbNsPW2P4jJfxvpxvkD8dhh6ZqqTEiVvMLAalTTZK
+
+// Virtual phone
+// eTpHtrUCSxO9JV1JHuJZka:APA91bHLICywKfi_gnKUc9AAR7h9r7vuCOShNBgGPzu8kSIWRhhYtYnQIVKb_HYrl1UgRpCziz36jUV-tEv94rDWUu0LphZDyy-ovtgkeXXu1Xxe9kQF_eBeMghZDje2RPemAiVfZxB5
 
 module.exports = { share };

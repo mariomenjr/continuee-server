e86ba70d2b3588a1f792595057f2d4e649e5674e
Mario Menjívar <mariomenjr@gmail.com>
2021-04-08 22:24:09 -0700

    Implement Docker for deployments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const packageJson = require("../../package.json");
 const firebaseRouter = require("./firebase.routes");
 
-router.use("/firebase", firebaseRouter)
+router.use(`/firebase`, firebaseRouter);
+router.use(`/`, (_, res) =>
+  res.send(`${packageJson.name}/${packageJson.version} @ ${new Date()}`)
+);
 
 module.exports = router;

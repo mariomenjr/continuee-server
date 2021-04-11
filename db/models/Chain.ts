80a968e11d3339e41227aff8bb64b7cd8caf7a4f
Mario Menjívar <mariomenjr@gmail.com>
2021-04-10 19:31:09 -0700

    Squashed commit of the following:
    commit 273ae58f8e35359291a79327064bb87d0581e3f5
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat Apr 10 19:29:12 2021 -0700
    
        Model Chain with Typescript
    
    commit ee4c76f16635f2ad5955d6f984b5defd6409cf02
    Merge: 962b056 a00da78
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat Apr 10 19:00:47 2021 -0700
    
        Merge branch 'main' of github.com:mariomenjr/continuee-server into feature/db
    
    commit 962b05699ea94deb5aee1e89abea8a08284b5593
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat Apr 10 18:55:59 2021 -0700
    
        Remove leftout from migration to Typescript
    
    commit c26dd6cd780f0f0eaab2631039f323fbd7a78a95
    Merge: 410e3f7 7ea858f
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat Apr 10 18:52:33 2021 -0700
    
        Merge branch 'main' of github.com:mariomenjr/continuee-server into feature/db
    
    commit 410e3f75ee23137d11ed52c8d3c59c3b088ca6b4
    Merge: f7e42d9 e6f29f5
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat Apr 10 18:45:58 2021 -0700
    
        Merge branch 'main' of github.com:mariomenjr/continuee-server into feature/db
    
    commit f7e42d9fc8d2c16e73cf3c72c65c9515c7c82464
    Merge: cd9cbc7 5023ff2
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat Apr 10 18:44:38 2021 -0700
    
        Merge branch 'main' of github.com:mariomenjr/continuee-server into feature/db
    
    commit cd9cbc7082de7cc9d62de49f6f7a1c23ccfd582d
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat Apr 10 15:47:46 2021 -0700
    
        Setup mongoose & create Chain model

--- db/models/Chain.ts
+++ db/models/Chain.ts
@@ -1,6 +1,6 @@
-import { Schema, models, model } from "mongoose";
+import mongoose, { Schema, Document, Model } from "mongoose";
 
-const chainSchema = new Schema(
+const ChainSchema = new Schema<ChainDocument, ChainModel>(
   {
     chainGuid: {
       type: Schema.Types.String,
@@ -14,6 +14,27 @@ const chainSchema = new Schema(
   { timestamps: true }
 );
 
-const Chain = models.Chain || model(`Chain`, chainSchema);
+export interface Chain {
+  chainGuid: string;
+  token: string;
+}
+
+/**
+ * Not directly exported because it is not recommanded to
+ * use this interface direct unless necessary since the related
+ * entities' fields are not deterministic
+ *
+ * See: Types, references, virtuals and instance methods
+ * https://medium.com/@agentwhs/complete-guide-for-typescript-for-mongoose-for-node-js-8cc0a7e470c1
+ */
+interface ChainDocument extends Chain, Document {} // ChainBaseDocument
+
+// export interface ChainDocument extends ChainBaseDocument {}
+
+export interface ChainModel extends Model<ChainDocument> {}
+
+const Chain =
+  mongoose.models.Chain ||
+  mongoose.model<ChainDocument, ChainModel>(`Chain`, ChainSchema);
 
 export default Chain;

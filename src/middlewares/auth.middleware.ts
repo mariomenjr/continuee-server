0fba694d96df871e723fbdabda4ef0d4d193e780
Mario Menjívar <mariomenjr@gmail.com>
2021-05-15 01:03:26 -0700

    Squashed commit of the following:
    commit b3e4b1296933a37d253b85028809d7a0979112a6
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat May 15 01:00:12 2021 -0700
    
        Verify bearer token against remote authority
    
    commit ab2b99ed8a8f38aec6de5c240aa64027ac3a8505
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Fri May 14 21:23:50 2021 -0700
    
        Distinguish protected rules from not protected ones
    
    commit 137c13ff1292716ab0c222388e12719777411162
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Fri May 14 21:11:26 2021 -0700
    
        Get Identity Issuer and Secret from .env
    
    commit 469809b3511c2063cd749925a03c89d36bd52e86
    Merge: f2b0a88 cd67d0f
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Fri May 14 21:08:37 2021 -0700
    
        Merge branch 'main' of github.com:mariomenjr/continuee-server into feature/identity
    
    commit f2b0a883667859db3651cd626b5bf1d92a5d9975
    Author: Mario Menjívar <mariomenjr@gmail.com>
    Date:   Sat May 8 19:45:44 2021 -0700
    
        Basic implementation of Passport to consume Identity Server

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,14 +1,38 @@
-import passport from "passport";
-import { Strategy, StrategyOptions, ExtractJwt } from "passport-jwt";
+import jwt from "express-jwt";
+import jwksRsa from "jwks-rsa";
 
-const opts = {} as StrategyOptions;
+const { IDENTITY_ISSUER, IDENTITY_SECRET } = process.env;
 
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = `secret`;
-opts.issuer = `identity.mariomenjr.com`;
+console.debug({ IDENTITY_ISSUER });
 
-passport.use(new Strategy(opts, function(payload, done) {
-  return done(null, false);
-}));
+const authorize = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${IDENTITY_ISSUER}/.well-known/openid-configuration/jwks`
+  }),
+  audience: `continuee_api`,
+  issuer: `${IDENTITY_ISSUER}`,
+  algorithms: [`RS256`]
+});
 
-export default passport;
+export default authorize;
+
+// import passport from "passport";
+// import { Strategy, StrategyOptions, ExtractJwt } from "passport-jwt";
+
+// const { IDENTITY_ISSUER, IDENTITY_SECRET } = process.env;
+
+// const opts = {} as StrategyOptions;
+
+// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// opts.secretOrKey = IDENTITY_SECRET;
+// // opts.issuer = IDENTITY_ISSUER;
+
+// passport.use(new Strategy(opts, function(payload, done) {
+//   console.debug({ payload });
+//   return done(null, false);
+// }));
+
+// export default passport;
